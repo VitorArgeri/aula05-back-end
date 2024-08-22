69ba5d2654e7d53b253f4643af5d3281d6560140
VitorArgeri <vitor.argeri@aluno.senai.br>
2024-08-22 17:13:20 -0300

    feat: Server created

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-"Vamos criar juntos"
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+
+const serverPort = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res.status(200).send({
+        message: "Hello World"
+    })
+})
+app.listen(serverPort, () => {
+    console.log(`😎 Server started on http://localhost:${serverPort}`)
+})
